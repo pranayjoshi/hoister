cd73dad29f8368d7b84855c28a899f9a2b084a51
pranayjoshi <pranayjoshi4466@gmail.com>
2024-02-28 00:24:05 -0600

    added routing to backend

---
+++ backend/app/app.go
@@ -0,0 +1,54 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
+)
+
+type App struct {
+	Router *mux.Router
+}
+
+func RegisterAPIRoutes(r *mux.Router) {
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}).Methods("GET")
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func (a *App) Start() error {
+	a.Router = mux.NewRouter()
+	RegisterAPIRoutes(a.Router)
+
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // All origins
+		AllowedMethods:   []string{"GET", "HEAD", "POST", "PUT", "OPTIONS"},
+		AllowedHeaders:   []string{"*"}, // All headers
+		AllowCredentials: true,
+	})
+
+	handler := c.Handler(a.Router)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	fmt.Println("Starting server on port " + port)
+
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+
+	return nil
+}
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,9 @@
 module github.com/pranayjoshi/hoister
 
 go 1.21.1
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+	github.com/rs/cors v1.10.1
+)
