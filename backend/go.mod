9d236a5a9f957d5b8f9ad071c6561a2a00bd5e0b
pranayjoshi <pranayjoshi4466@gmail.com>
2024-03-09 12:21:17 -0600

    updated

--- backend/go.mod
+++ backend/go.mod
@@ -10,14 +10,22 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
+
+require (
 	dario.cat/mergo v1.0.0 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/ProtonMail/go-crypto v0.0.0-20230828082145-3c4c8a2d2371 // indirect
+	github.com/aws/aws-sdk-go v1.50.29
 	github.com/cloudflare/circl v1.3.3 // indirect
 	github.com/cyphar/filepath-securejoin v0.2.4 // indirect
 	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.5.0 // indirect
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
--- backend/services/upload/upload.go
+++ backend/services/upload/upload.go
@@ -1,18 +1,83 @@
 package upload
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 
-	"github.com/go-git/go-git/v5"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/go-redis/redis/v8"
 )
 
-func CloneRepo() {
+var ctx = context.Background()
 
-	_, err := git.PlainClone("/tmp/foo", false, &git.CloneOptions{
-		URL:      "https://github.com/go-git/go-git",
-		Progress: os.Stdout,
+func main() {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
 	})
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2"),
+		Credentials: credentials.NewStaticCredentials(
+			os.Getenv("AWS_ACCESS_KEY_ID"),
+			os.Getenv("AWS_SECRET_ACCESS_KEY"),
+			"",
+		),
+	}))
+
+	uploader := s3manager.NewUploader(sess)
+
+	outDirPath := "./output"
+
+	cmd := exec.Command("sh", "-c", fmt.Sprintf("cd %s && npm install && npm run build", outDirPath))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 	if err != nil {
-		panic(err)
+		fmt.Println("Error", err)
+		return
 	}
+
+	err = filepath.Walk(outDirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+
+			defer file.Close()
+
+			_, err = uploader.Upload(&s3manager.UploadInput{
+				Bucket: aws.String("vercel-clone-outputs"),
+				Key:    aws.String(fmt.Sprintf("__outputs/%s/%s", "PROJECT_ID", strings.TrimPrefix(path, outDirPath))),
+				Body:   file,
+			})
+
+			if err != nil {
+				return err
+			}
+
+			fmt.Println("Successfully uploaded", path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println("Error", err)
+		return
+	}
+
+	fmt.Println("Done...")
 }
