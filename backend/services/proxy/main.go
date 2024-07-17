0965ef1451e04c412f840115c54d50878eb4bb25
pranayjoshi <pranayjoshi4466@gmail.com>
2024-07-17 03:00:17 -0500

    updated proxy: working

--- backend/services/proxy/main.go
+++ backend/services/proxy/main.go
@@ -24,7 +24,6 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	hostname := r.Host
 	subdomain := strings.Split(hostname, ".")[0]
 
-	// Custom Domain - DB Query
 	resolvesTo := fmt.Sprintf("%s%s", BASE_PATH, subdomain)
 	fmt.Println("Resolves to: ", resolvesTo)
 	target, err := url.Parse(resolvesTo)
@@ -37,30 +36,43 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	proxy := httputil.NewSingleHostReverseProxy(target)
 	proxy.ModifyResponse = func(resp *http.Response) error {
 		resp.Header.Set("Content-Disposition", "inline")
-		return nil
-	}
-	// }
-	proxy.ModifyResponse = func(resp *http.Response) error {
-		if strings.HasSuffix(resp.Request.URL.Path, ".html") {
-			resp.Header.Set("Content-Type", "text/html")
+		contentType := "application/octet-stream"
+		if strings.HasSuffix(resp.Request.URL.Path, ".svg") {
+			contentType = "image/svg+xml"
+		} else if strings.HasSuffix(resp.Request.URL.Path, ".html") {
+			contentType = "text/html"
 		} else if strings.HasSuffix(resp.Request.URL.Path, ".js") {
-			resp.Header.Set("Content-Type", "application/javascript")
+			contentType = "application/javascript"
 		} else if strings.HasSuffix(resp.Request.URL.Path, ".css") {
-			resp.Header.Set("Content-Type", "text/css")
+			contentType = "text/css"
 		}
+		resp.Header.Set("Content-Type", contentType)
 		return nil
 	}
 	proxy.Director = func(req *http.Request) {
 		req.URL.Host = target.Host
 		req.URL.Scheme = target.Scheme
 		req.Host = target.Host
-		// req.URL.Path += ""
-		req.URL.Path = strings.TrimPrefix(req.URL.Path, "/")
-		fmt.Println("Path: ", req.URL.Path, "Target Path: ", target.Path)
-		req.URL.Path = target.Path + "/" + req.URL.Path
-
-		fmt.Println("Proxyaaaing to: ", req.URL)
+		originalPath := strings.TrimPrefix(req.URL.Path, "/")
+		if originalPath == "favicon.ico" {
+			http.NotFound(w, req)
+			return
+		}
+		req.URL.Path = singleJoiningSlash(target.Path, originalPath)
+		fmt.Println("Proxying to: ", req.URL)
 	}
 	fmt.Println("Proxying to: ", target)
 	proxy.ServeHTTP(w, r)
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
