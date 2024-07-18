63961b7a494906adc61dc5f940221633a89429cf
pranayjoshi <pranayjoshi4466@gmail.com>
2024-07-18 07:32:00 -0500

    first draft deploy server

--- backend/services/deploy/go.mod
+++ backend/services/deploy/go.mod
@@ -1,3 +1,31 @@
 module github.com/pranayjoshi/hoister/backend/services/deploy
 
 go 1.22.0
+
+require github.com/aws/aws-sdk-go-v2 v1.30.3
+
+require (
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.26 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.11 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.17 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.22.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.30.3 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2/config v1.27.26
+	github.com/aws/aws-sdk-go-v2/service/ecs v1.44.3
+	github.com/aws/smithy-go v1.20.3 // indirect
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.3
+)
---
+++ backend/services/deploy/main.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
+	"github.com/go-redis/redis/v8"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+// const (
+
+// )
+
+var (
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	redisClient *redis.Client
+	ecsClient   *ecs.Client
+)
+
+type ProjectRequest struct {
+	GitURL string `json:"gitURL"`
+	Slug   string `json:"slug"`
+}
+
+func main() {
+	PORT := os.Getenv("RUN_PORT")
+	REDIS_ADDR := ""
+	AWS_REGION := ""
+	AWS_ACCESS_KEY := ""
+	AWS_SECRET_KEY := ""
+
+	router := mux.NewRouter()
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: REDIS_ADDR,
+	})
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(AWS_REGION),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(AWS_ACCESS_KEY, AWS_SECRET_KEY, "")),
+	)
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	ecsClient = ecs.NewFromConfig(cfg)
+
+	router.HandleFunc("/project", handleProjectCreation).Methods("POST")
+	go startWebSocketServer()
+	go initRedisSubscribe()
+
+	log.Printf("API Server Running on port %s", PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, router))
+}
+
+func handleProjectCreation(w http.ResponseWriter, r *http.Request) {
+	ECS_CLUSTER := ""
+	ECS_TASK := ""
+	var req ProjectRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	projectSlug := req.Slug
+	if projectSlug == "" {
+		projectSlug = uuid.New().String()
+	}
+
+	_, err = ecsClient.RunTask(context.TODO(), &ecs.RunTaskInput{
+		Cluster:        aws.String(ECS_CLUSTER),
+		TaskDefinition: aws.String(ECS_TASK),
+		LaunchType:     "FARGATE",
+		Count:          aws.Int32(1),
+		NetworkConfiguration: &ecs.NetworkConfiguration{
+			AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
+				AssignPublicIp: aws.String("ENABLED"),
+				Subnets:        []string{"", "", ""},
+				SecurityGroups: []string{""},
+			},
+		},
+		Overrides: &ecs.TaskOverride{
+			ContainerOverrides: []ecs.ContainerOverride{
+				{
+					Name: aws.String("builder-image"),
+					Environment: []ecs.KeyValuePair{
+						{Name: aws.String("GIT_REPOSITORY__URL"), Value: aws.String(req.GitURL)},
+						{Name: aws.String("PROJECT_ID"), Value: aws.String(projectSlug)},
+					},
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"status": "queued",
+		"data": map[string]string{
+			"projectSlug": projectSlug,
+			"url":         fmt.Sprintf("http://%s.localhost:8000", projectSlug),
+		},
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func startWebSocketServer() {
+	SOCKET_PORT := os.Getenv("SOCKET_PORT")
+	router := mux.NewRouter()
+	router.HandleFunc("/ws", handleWebSocket)
+	log.Printf("WebSocket Server Running on port %s", SOCKET_PORT)
+	log.Fatal(http.ListenAndServe(":"+SOCKET_PORT, router))
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		var message struct {
+			Action  string `json:"action"`
+			Channel string `json:"channel"`
+		}
+		err = json.Unmarshal(p, &message)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		if message.Action == "subscribe" {
+			err = conn.WriteMessage(messageType, []byte(fmt.Sprintf("Joined %s", message.Channel)))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}
+
+func initRedisSubscribe() {
+	pubsub := redisClient.PSubscribe(context.Background(), "logs:*")
+	defer pubsub.Close()
+
+	log.Println("Subscribed to logs....")
+
+	ch := pubsub.Channel()
+	for msg := range ch {
+		// Broadcast message to all connected WebSocket clients
+		// This part is simplified and would need to be implemented
+		// based on your WebSocket management strategy
+		log.Printf("Received message: %s", msg.Payload)
+	}
+}
